007115f3a952da6828f0b9efb0b9ca76dd3dbfeb
Saurabh <92sharmasaurabh@gmail.com>
2015-10-12 00:36:03 +0530

    path of images in landing page

--- scripts/controllers/landingPageController.js
+++ scripts/controllers/landingPageController.js
@@ -3,15 +3,15 @@ myApp.controller('landingPageController', function($scope) {
 	
 	$scope.accordionData = [{
 		open:true,
-		imgSrc: '/images/greedyGame1.jpg'
+		imgSrc: 'images/greedyGame1.jpg'
 	},
 	{
 		open:false,
-		imgSrc: '/images/greedyGame2.jpg'
+		imgSrc: 'images/greedyGame2.jpg'
 	},
 	{
 		open:false,
-		imgSrc: '/images/greedyGame3.png'
+		imgSrc: 'images/greedyGame3.png'
 	}
 	];
   
